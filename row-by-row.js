8e1b45d79c1cb661e172ce731ee5c32595921b2b
Rico Chen <ricochen27@gmail.com>
2017-12-25 12:57:40 -0500

    lint

--- row-by-row.js
+++ row-by-row.js
@@ -3,18 +3,20 @@ const Settings = require('./settings')
 const db = require('./db')
 
 const run = async () => {
-	console.time('prepare')
-	const records = Array(Settings.maxRows).fill().map((e,i)=>[i+1, `user${i}`]);
-	console.timeEnd('prepare')
+    console.time('prepare')
+    const records = Array(Settings.maxRows)
+        .fill()
+        .map((e, i) => [i + 1, `user${i}`])
+    console.timeEnd('prepare')
 
-	console.time('insert')
-	const conn = await db()
-	const stmt = `insert into ${Settings.table} values ?`
-	await conn.query(`truncate table ${Settings.table}`)
-	await Promise.each(records, r => {
-		return conn.query(stmt, [[r]])
-	})
-	await conn.end()
-	console.timeEnd('insert')
+    console.time('insert')
+    const conn = await db()
+    const stmt = `insert into ${Settings.table} values ?`
+    await conn.query(`truncate table ${Settings.table}`)
+    await Promise.each(records, r => {
+        return conn.query(stmt, [[r]])
+    })
+    await conn.end()
+    console.timeEnd('insert')
 }
 run()
